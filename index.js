d8c5ee90a06a3149ef6585b5b40ee2722bdb9b13
Alex Thiga <loydeflacko@gmail.com>
2022-06-07 11:08:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+// multiply num1 and num2 to give 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2
+console.log( multiply )
+
+// random number greater than 0
+const random = Math.floor( Math.random() * 10 ) + 1
+
+// mod of num3 and num4 to give remainder 4
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4
+
+// display max value
+const max = Math.max(1,2,14,20)
